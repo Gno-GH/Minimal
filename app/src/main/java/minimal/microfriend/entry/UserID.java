c6356f437c485071c7e92e29f84b0dc4e43d056b
Gno <1835678160@qq.com>
2016-06-12 19:37:35 +0800

    完善实体类

--- app/src/main/java/minimal/microfriend/entry/UserID.java
+++ app/src/main/java/minimal/microfriend/entry/UserID.java
@@ -1,7 +1,27 @@
 package minimal.microfriend.entry;
 
+import cn.bmob.v3.BmobObject;
+
 /**
  * Created by gno on 16/6/12.
  */
-public class UserID {
+public class UserID extends BmobObject{
+    private String userid;
+    private School school;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public School getSchool() {
+        return school;
+    }
+
+    public void setSchool(School school) {
+        this.school = school;
+    }
 }
